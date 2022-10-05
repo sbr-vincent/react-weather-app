3c9c9d2e4d7e2632394cd15dc362bcf57591acd8
sbr-vincent <vincent.g.sbr@gmail.com>
2022-10-05 13:41:27 -0700

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '42cb341f78msh921fe001a18c793p1a71aajsndea862af9f16',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
